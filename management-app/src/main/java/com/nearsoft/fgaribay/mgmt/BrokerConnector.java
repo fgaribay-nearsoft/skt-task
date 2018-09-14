636b931296e1bc1da23cb1925226517d54496301
Fernando R Garibay <fgaribay@nearsoft.com>
2018-09-13 22:11:26 -0700

    Use type-safe configuration properties

--- management-app/src/main/java/com/nearsoft/fgaribay/mgmt/BrokerConnector.java
+++ management-app/src/main/java/com/nearsoft/fgaribay/mgmt/BrokerConnector.java
@@ -1,11 +1,11 @@
 package com.nearsoft.fgaribay.mgmt;
 
+import com.nearsoft.fgaribay.mgmt.config.ProductProperties;
 import com.nearsoft.fgaribay.mgmt.model.Product;
 import com.nearsoft.fgaribay.mgmt.model.ProductRequest;
 import com.nearsoft.fgaribay.mgmt.model.ProductResponse;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -13,21 +13,21 @@ import java.util.List;
 
 @Component
 public class BrokerConnector {
+
     private final RabbitTemplate rabbitTemplate;
-    @Value("product.create.queue.name")
-    private String productCreationQueueName;
-    @Value("product.list.queue.name")
-    private String productListQueueName;
+
+    private ProductProperties.Queues queueProperties;
 
     @Autowired
-    public BrokerConnector(RabbitTemplate rabbitTemplate) {
+    public BrokerConnector(RabbitTemplate rabbitTemplate, ProductProperties properties) {
         this.rabbitTemplate = rabbitTemplate;
+        this.queueProperties = properties.getQueues();
     }
 
     public List<Product> retrieveProductList() {
         ProductRequest request = new ProductRequest("list", null);
         ProductResponse response =
-                (ProductResponse) rabbitTemplate.convertSendAndReceive(productListQueueName, request);
+                (ProductResponse) rabbitTemplate.convertSendAndReceive(queueProperties.getListName(), request);
 
         if (response == null) {
             throw new RuntimeException(
@@ -42,7 +42,7 @@ public class BrokerConnector {
     public void createProduct(Product product) {
         ProductRequest request = new ProductRequest("create", Collections.singletonList(product));
         ProductResponse response =
-                (ProductResponse) rabbitTemplate.convertSendAndReceive(productCreationQueueName, request);
+                (ProductResponse) rabbitTemplate.convertSendAndReceive(queueProperties.getCreationName(), request);
 
         if (response == null) {
             throw new RuntimeException(
--- management-app/src/main/java/com/nearsoft/fgaribay/mgmt/config/BrokerConfig.java
+++ management-app/src/main/java/com/nearsoft/fgaribay/mgmt/config/BrokerConfig.java
@@ -2,25 +2,27 @@ package com.nearsoft.fgaribay.mgmt.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BrokerConfig {
-    @Value("product.create.queue.name")
-    private String productCreationQueueName;
 
-    @Value("product.list.queue.name")
-    private String productListQueueName;
+    private ProductProperties.Queues queueProperties;
+
+    @Autowired
+    public BrokerConfig(ProductProperties properties) {
+        this.queueProperties = properties.getQueues();
+    }
 
     @Bean
     Queue productCreationQueue() {
-        return QueueBuilder.durable(productCreationQueueName).build();
+        return QueueBuilder.durable(queueProperties.getCreationName()).build();
     }
 
     @Bean
     Queue productListQueue() {
-        return QueueBuilder.durable(productListQueueName).build();
+        return QueueBuilder.durable(queueProperties.getListName()).build();
     }
 }
