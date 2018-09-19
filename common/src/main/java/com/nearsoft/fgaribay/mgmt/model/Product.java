4eb203c266b2afce57b9991c78786f337dcfa22c
Fernando R Garibay <fgaribay@nearsoft.com>
2018-09-18 17:58:26 -0700

    Reformat code in common

--- common/src/main/java/com/nearsoft/fgaribay/mgmt/model/Product.java
+++ common/src/main/java/com/nearsoft/fgaribay/mgmt/model/Product.java
@@ -3,45 +3,44 @@ package com.nearsoft.fgaribay.mgmt.model;
 import java.io.Serializable;
 
 public class Product implements Serializable {
-    public void setId(long id) {
-        this.id = id;
-    }
+  private long id;
+  private String name;
+  private String description;
 
-    private long id;
-    private String name;
-    private String description;
+  public Product() {}
 
-    public Product() {
-    }
+  public Product(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
 
-    public Product(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
+  public Product(long id, String name, String description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
 
-    public Product(long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public long getId() {
-        return id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 }
--- common/src/main/java/com/nearsoft/fgaribay/mgmt/model/ProductRequest.java
+++ common/src/main/java/com/nearsoft/fgaribay/mgmt/model/ProductRequest.java
@@ -5,45 +5,44 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class ProductRequest implements Serializable {
-    private UUID id;
-    private String operation;
-    private ArrayList<Product> products;
+  private UUID id;
+  private String operation;
+  private ArrayList<Product> products;
 
-    public ProductRequest(UUID id, String operation, ArrayList<Product> products) {
-        this.id = id;
-        this.operation = operation;
-        this.products = products;
-    }
+  public ProductRequest(UUID id, String operation, ArrayList<Product> products) {
+    this.id = id;
+    this.operation = operation;
+    this.products = products;
+  }
 
-    public ProductRequest() {
-    }
+  public ProductRequest() {}
 
-    public ProductRequest(String operation, ArrayList<Product> products) {
-        this.operation = operation;
-        this.products = products;
-    }
+  public ProductRequest(String operation, ArrayList<Product> products) {
+    this.operation = operation;
+    this.products = products;
+  }
 
-    public String getOperation() {
-        return operation;
-    }
+  public String getOperation() {
+    return operation;
+  }
 
-    public void setOperation(String operation) {
-        this.operation = operation;
-    }
+  public void setOperation(String operation) {
+    this.operation = operation;
+  }
 
-    public UUID getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 
-    public ArrayList<Product> getProducts() {
-        return products;
-    }
+  public ArrayList<Product> getProducts() {
+    return products;
+  }
 
-    public void setProducts(ArrayList<Product> products) {
-        this.products = products;
-    }
+  public void setProducts(ArrayList<Product> products) {
+    this.products = products;
+  }
 }
