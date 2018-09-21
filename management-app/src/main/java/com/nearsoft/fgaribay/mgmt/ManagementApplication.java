ad69bbaa6c7de186757a6d65344f4d0ba0d73a9b
Fernando R Garibay <fgaribay@nearsoft.com>
2018-09-21 11:04:39 -0700

    Use Spring profile for common properties (missed file)
    Continues previous commit. Missed file ManagementApplication.java

--- management-app/src/main/java/com/nearsoft/fgaribay/mgmt/ManagementApplication.java
+++ management-app/src/main/java/com/nearsoft/fgaribay/mgmt/ManagementApplication.java
@@ -4,10 +4,8 @@ import com.nearsoft.fgaribay.mgmt.config.BrokerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:common.properties")
 @Import(BrokerConfig.class)
 public class ManagementApplication {
 
